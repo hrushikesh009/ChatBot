beb84443f96566a7af99baa27ce356d0899f96b2
hrushikesh_09 <hrushimalpekar1999@gmail.com>
2021-02-11 20:56:09 +0000

    Added form Feature

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,41 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List, Union
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet, EventType
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ValidateHealthForm(Action):
+
+    def name(self) -> Text:
+        return "health_form"
+    
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+        ) -> List[EventType]:
+            requried_slots = ["confirm_exercise","exercise","sleep","stress","diet","goal"]
+
+            for slot_name in requried_slots:
+                if tracker.slots.get(slot_name) is None:
+                    return[SlotSet("requested_slot", slot_name)]
+            
+            return[SlotSet("requested_slot", "None")]
+
+class ActionSubmit(Action):
+
+    def name(self) -> Text:
+        return "action_submit"
+
+    def run(
+        self, 
+        dispatcher,
+        tracker:Tracker,
+        domain: "DomainDict"
+        ) -> List[Dict[Text, Any]]:
+             dispatcher.utter_message(template="utter_slots_values",
+             confirm_exercise = tracker.get_slot("confirm_exercise"),
+             exercise = tracker.get_slot("exercise"),
+             sleep = tracker.get_slot("sleep"),
+             stress = tracker.get_slot("stress"),
+             diet = tracker.get_slot("diet"),
+             goal = tracker.get_slot("goal"))
