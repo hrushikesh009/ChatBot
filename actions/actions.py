fb9f4cb9dda3bf23947321326f1189937b7f3194
hrushikesh_09 <hrushimalpekar1999@gmail.com>
2021-04-18 18:15:08 +0000

    Modified files

--- actions/actions.py
+++ actions/actions.py
@@ -16,15 +16,15 @@ airtable_api_key = os.getenv("AIRTABLE_API_KEY")
 base_id = os.getenv("BASE_ID")
 table_name = os.getenv("TABLE_NAME")
 
+
 def create_health_log(confirm_exercise, exercise, sleep, diet, stress, goal):
-    request_url=f"https://api.airtable.com/v0/{base_id}/{table_name}?api_key={airtable_api_key}"
-    
+    request_url = f"https://api.airtable.com/v0/{base_id}/{table_name}?api_key={airtable_api_key}"
 
     headers = {
         "Content-Type": "application/json",
         "Accept": "application/json",
         "Authorization": f"Bearer {airtable_api_key}",
-    }  
+    }
     data = {
         "fields": {
             "Exercised?": confirm_exercise,
@@ -42,26 +42,28 @@ def create_health_log(confirm_exercise, exercise, sleep, diet, stress, goal):
         response.raise_for_status()
     except requests.exceptions.HTTPError as err:
         raise SystemExit(err)
-    
+
     print(response.status_code)
     return response
-    
+
+
 class ValidateHealthForm(Action):
 
     def name(self) -> Text:
         return "health_form"
-    
+
     def run(
         self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
-        ) -> List[EventType]:
-            requried_slots = ["confirm_exercise","exercise","sleep","stress","diet","goal"]
+    ) -> List[EventType]:
+        requried_slots = ["confirm_exercise", "exercise",
+                          "sleep", "stress", "diet", "goal"]
 
-            for slot_name in requried_slots:
-                if tracker.slots.get(slot_name) is None:
-                    return[SlotSet("requested_slot", slot_name)]
-            
-            return[SlotSet("requested_slot", "None")]
-    
+        for slot_name in requried_slots:
+            if tracker.slots.get(slot_name) is None:
+                return[SlotSet("requested_slot", slot_name)]
+
+        return[SlotSet("requested_slot", "None")]
+
 
 class ActionSubmit(Action):
 
@@ -69,27 +71,28 @@ class ActionSubmit(Action):
         return "action_submit"
 
     def run(
-        self, 
+        self,
         dispatcher,
-        tracker:Tracker,
+        tracker: Tracker,
         domain: "DomainDict"
-        ) -> List[Dict[Text, Any]]:
-             
-            confirm_exercise = tracker.get_slot("confirm_exercise")
-            exercise = tracker.get_slot("exercise")
-            sleep = tracker.get_slot("sleep")
-            stress = tracker.get_slot("stress")
-            diet = tracker.get_slot("diet")
-            goal = tracker.get_slot("goal")
+    ) -> List[Dict[Text, Any]]:
 
-            response = create_health_log(
-                confirm_exercise=confirm_exercise,
-                exercise=exercise,
-                sleep=sleep,
-                stress=stress,
-                diet=diet,
-                goal=goal
-            )
+        confirm_exercise = tracker.get_slot("confirm_exercise")
+        exercise = tracker.get_slot("exercise")
+        sleep = tracker.get_slot("sleep")
+        stress = tracker.get_slot("stress")
+        diet = tracker.get_slot("diet")
+        goal = tracker.get_slot("goal")
 
-            dispatcher.utter_message(text = "Thanks, your answers have been recorded!")
-            return []
+        response = create_health_log(
+            confirm_exercise=confirm_exercise,
+            exercise=exercise,
+            sleep=sleep,
+            stress=stress,
+            diet=diet,
+            goal=goal
+        )
+
+        dispatcher.utter_message(
+            text="Thanks, your answers have been recorded!")
+        return []
